31612afc57ebe856ed33ba31f3fab4928e8fe3c0
Jeremy <71183843+MyNameJeremy@users.noreply.github.com>
2021-01-03 17:18:29 +0100

    Uploaded the finished Project (en and de)
    I coded the original project in german but quickly translated all variable names and user inputs to english and uploaded it as a separate file to make it more accessible to the public. Now there are two python files: LotteryGerman.py (the original german version) and LotteryEnglish.py (the english version)

---
+++ LotteryEnglish.py
@@ -0,0 +1,61 @@
+import random
+
+correct = 0
+total = 0
+run = 1
+
+
+howmany = int(input("How many correct guesses should there be? (0-6) "))
+if howmany < 0:
+    print("That was below 0.")
+    quit()
+elif howmany > 6:
+    print("That was over 6.")
+    quit()
+
+runs = int(input("How many runs should there be? "))
+if runs < 1:
+    print("There has to be at least one run.")
+    quit()
+
+
+while run <= runs:
+    correct = 0
+    howoften = 0
+    while correct != howmany:
+        correct = 0
+        guess = []
+        drawing = []
+
+        while len(guess) < 6:
+            if len(guess) < 6:
+                number = random.randint(1, 50)
+                if not number in guess:
+                    guess.append(number)
+        while len(drawing) < 6:
+            if len(drawing) < 6:
+                number2 = random.randint(1, 50)
+                if not number2 in drawing:
+                    drawing.append(number2)
+
+        x = 0
+        y = 0
+        while y < len(guess):
+            while x < len(guess):
+                if guess[x] == drawing[y]:
+                    correct += 1
+                x += 1
+            y += 1
+            x = 0
+        howoften += 1
+        print("run", (run), "| draw ",
+              howoften, "| correct", correct)
+    total += howoften
+    run += 1
+
+
+average = round((total / runs), 2)
+
+
+print("To get", howmany, "correct guesses", runs, "times, there had to be a total of", total, "tries. That's an average of ", average, "tries per run. (so the chance is 1:" + str(average) + ")")
+input("Hit Enter to exit")
---
+++ LotteryGerman.py
@@ -0,0 +1,63 @@
+import random
+
+richtige = 0
+gesamt = 0
+durchlauf = 1
+
+
+wieviele = int(input("Wie viele richtige soll es geben? (0-6) "))
+if wieviele < 0:
+    print("Das war unter 0.")
+    quit()
+elif wieviele > 6:
+    print("Das war über 6.")
+    quit()
+
+durchlaufe = int(input("Wie viele Durchläufe, soll es geben? "))
+if durchlaufe < 1:
+    print("Es muss mindestens einen Durchlauf geben.")
+    quit()
+
+
+while durchlauf <= durchlaufe:
+    richtige = 0
+    wieoft = 0
+    while richtige != wieviele:
+        richtige = 0
+        tipp = []
+        ziehung = []
+
+        while len(tipp) < 6:
+            if len(tipp) < 6:
+                zahl = random.randint(1, 50)
+                if not zahl in tipp:
+                    tipp.append(zahl)
+        while len(ziehung) < 6:
+            if len(ziehung) < 6:
+                zahl2 = random.randint(1, 50)
+                if not zahl2 in ziehung:
+                    ziehung.append(zahl2)
+
+        x = 0
+        y = 0
+        while y < len(tipp):
+            while x < len(tipp):
+                if tipp[x] == ziehung[y]:
+                    richtige += 1
+                x += 1
+            y += 1
+            x = 0
+        wieoft += 1
+        print("Durchlauf", (durchlauf), "| Ziehung ",
+              wieoft, "| Richtige", richtige)
+    gesamt += wieoft
+    durchlauf += 1
+
+
+durchschnitt = round((gesamt / durchlaufe), 2)
+
+
+
+print("Um", durchlaufe, "mal", wieviele, "richtige zu bekommen, musste insgesamt",
+      gesamt, "mal gezogen werden. Das sind im Durschschnitt", durchschnitt, "mal. (also ist die Chance 1:" + str(durchschnitt) + ")")
+input("Drücke Enter zum beenden")
